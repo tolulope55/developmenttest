26c68b1c4a0bf3b6619861e9c78094accab2ca71
tolulope55 <olonts@gmail.com>
2022-10-17 07:39:06 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DevelopmentTest from './component/DevelopmentTest';
+
+function App() {
+  return (
+    <div className="App">
+     <DevelopmentTest />
+    </div>
+  );
+}
+
+export default App;
